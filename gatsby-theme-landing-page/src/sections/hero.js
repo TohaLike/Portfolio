a1296970ca1d9ecfcb9374152a703ebe7f98f2f9
Anton <thetoxalike444@gmail.com>
2023-08-03 02:33:41 +0300

    first commit

--- gatsby-theme-landing-page/src/sections/hero.js
+++ gatsby-theme-landing-page/src/sections/hero.js
@@ -7,43 +7,43 @@ import MarkdownText from "../components/markdown-text";
 import Heading from "../components/heading";
 
 export default function Hero({ heading, secondaryHeading, content }) {
-  const heroContent = content?.[0];
-  const image = getImage(heroContent?.image);
+   const heroContent = content?.[0];
+   const image = getImage(heroContent?.image);
 
-  return (
-    <Section>
-      <div className={styles.root}>
-        <div className={styles.content}>
-          <Heading as="h2" className={styles.secondaryHeading}>
-            {secondaryHeading}
-          </Heading>
-          <Heading as="h1" className={styles.heading}>
-            {heading}
-          </Heading>
-          <HeroContent {...heroContent} />
-        </div>
-        <div className={styles.image}>
-          <GatsbyImage image={image} alt={image.title || `Hero Image`} />
-        </div>
-      </div>
-    </Section>
-  );
+   return (
+      <Section>
+         <div className={styles.root}>
+            <div className={styles.content}>
+               <Heading as="h1" className={styles.heading}>
+                  {heading}
+               </Heading>
+               <Heading as="h2" className={styles.secondaryHeading}>
+                  {secondaryHeading}
+               </Heading>
+               <HeroContent {...heroContent} />
+            </div>
+            <div className={styles.image}>
+               <GatsbyImage image={image} alt={image.title || `Hero Image`} />
+            </div>
+         </div>
+      </Section>
+   );
 }
 
 function HeroContent({ primaryText, secondaryText, links }) {
-  return (
-    <div>
-      <MarkdownText {...primaryText} />
-      <MarkdownText {...secondaryText} />
-      <div className={styles.buttonContainer}>
-        {links.map((link, i) => (
-          <Button
-            key={link.id}
-            {...link}
-            variant={i === 0 ? "primary" : "secondary"}
-          />
-        ))}
+   return (
+      <div>
+         <MarkdownText {...primaryText} />
+         <MarkdownText {...secondaryText} />
+         <div className={styles.buttonContainer}>
+            {links.map((link, i) => (
+               <Button
+                  key={link.id}
+                  {...link}
+                  variant={i === 0 ? "primary" : "secondary"}
+               />
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   );
 }
