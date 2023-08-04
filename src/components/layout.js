e2a48a4896f0f2e0cd620dc6f27edb284232430c
Anton <thetoxalike444@gmail.com>
2023-08-04 17:37:06 +0300

    I added new content in hero's section

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,20 +6,20 @@ import * as cssVars from "gatsby-theme-landing-page/src/styles/variables.module.
 import * as styles from "./layout.module.css";
 
 export default function Layout(props) {
-   return (
-      <div className={[cssVars.root, styles.root].join(" ")}>
-         <Head {...props} />
-         <header className={styles.header}>
-            <Link to="/">gatsby-starter-landing-page</Link>
-         </header>
-         <main className={styles.main}>{props.children}</main>
-         <footer className={styles.footer}>
-            <a href="https://gatsbyjs.com">Built with Gatsby</a>
-            <a href="https://github.com/gatsbyjs/gatsby-starter-landing-page">
-               View Source on GitHub
-            </a>
-            <Link to="/demo">Demo</Link>
-         </footer>
-      </div>
-   );
+  return (
+    <div className={[cssVars.root, styles.root].join(" ")}>
+      <Head {...props} />
+      <header className={styles.header}>
+        {/* <Link to="/">gatsby-starter-landing-page</Link> */}
+      </header>
+      <main className={styles.main}>{props.children}</main>
+      <footer className={styles.footer}>
+        <a href="https://gatsbyjs.com">Built with Gatsby</a>
+        <a href="https://github.com/gatsbyjs/gatsby-starter-landing-page">
+          View Source on GitHub
+        </a>
+        <Link to="/demo">Demo</Link>
+      </footer>
+    </div>
+  );
 }
