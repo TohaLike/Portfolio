ac262ac1df989a94a3ebc063ca221881c4d149c7
Anton <thetoxalike444@gmail.com>
2023-08-10 17:10:31 +0300

    I added new style to image

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ export default function Layout(props) {
         <a href="https://github.com/gatsbyjs/gatsby-starter-landing-page">
           View Source on GitHub
         </a>
-        <Link to="/demo">Demo</Link>
+        {/* <Link to="/demo">Demo</Link> */}
       </footer>
     </div>
   );
